df82fda10fae5612ee5f3ae3c315a0026d67b72a
Luke Cachia <lukecachia@gmail.com>
2019-12-07 13:19:19 +0100

    Added an Extended Floating Action Buttons. Added the functionality to the main activity to hide/unhide buttons on click.

--- app/src/main/java/com/example/assignment2/main/MainActivity.java
+++ app/src/main/java/com/example/assignment2/main/MainActivity.java
@@ -5,20 +5,44 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import android.os.Bundle;
+import android.view.View;
 
 
 import com.example.assignment2.R;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final ExtendedFloatingActionButton fabMain        = findViewById(R.id.mainFab);
+        final ExtendedFloatingActionButton fabAddBike     = findViewById(R.id.mainAddBike);
+        final ExtendedFloatingActionButton fabAddActivity = findViewById(R.id.mainAddActivity);
+
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen == true){
+                    fabAddBike.setVisibility(View.INVISIBLE);
+                    fabAddActivity.setVisibility(View.INVISIBLE);
+                    isOpen = false;
+                } else {
+                    fabAddBike.setVisibility(View.VISIBLE);
+                    fabAddActivity.setVisibility(View.VISIBLE);
+                    isOpen = true;
+                }
+            }
+        });
+
+
     }
 }
