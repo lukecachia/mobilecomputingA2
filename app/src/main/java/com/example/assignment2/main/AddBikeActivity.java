4f0726f801f3862f5655eac97358693ec60d26a9
lukecachia <lukecachia@gmail.com>
2019-12-12 15:14:43 +0100

    Added photo capture functionality. Created the Activity_add_bike_photo XML layout file which contains the design which contains the camera window and capture button. On the other hand, the AddBikePhotoActivity Java class, contains all the methods which are required for the Camera2 API to work. This contains methods which allow the app to request permissions and actually capture a photo.

--- app/src/main/java/com/example/assignment2/main/AddBikeActivity.java
+++ app/src/main/java/com/example/assignment2/main/AddBikeActivity.java
@@ -2,8 +2,10 @@ package com.example.assignment2.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.assignment2.R;
@@ -14,21 +16,34 @@ import java.util.Date;
 
 public class AddBikeActivity extends AppCompatActivity {
 
+    Button photoBtn;
+
     private TextView makeTxtView;
     private TextView modelTxtView;
     private TextView dopTxtView;
 
-    //Getting todays date
+    //Getting today's date
     Date date = Calendar.getInstance().getTime();
     SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
     String todaysDate = formatter.format(date);
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_bike);
+
+        //Photo button
+        photoBtn = findViewById(R.id.addBikePhoto);
+
+        //Calls method which opens add photo act
+        photoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddPhotoActivity();
+            }
+        });
+
 
         makeTxtView  = findViewById(R.id.bikeBrand);
         modelTxtView = findViewById(R.id.bikeModel);
@@ -51,10 +66,14 @@ public class AddBikeActivity extends AppCompatActivity {
         //Auto filling the date in the text view
         dopTxtView.setText(todaysDate);
 
-
-
     }
 
+    //method which opens the addbikephoto activity
+    private void openAddPhotoActivity() {
+        Intent intent = new Intent(this, AddBikePhotoActivity.class);
+        startActivity(intent);
+
+    }
 
 
 }
