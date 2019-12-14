13b2ea683e3b7fb7fc88ec9d161dcf0b6e8e60c2
Luke Cachia <lukecachia@gmail.com>
2019-12-14 12:54:02 +0100

    Continued working on the "Add Activity" page. Added the textview, spinner (dropdown) and button. Currently, dropdown list is hardcoded and just shows a Toast.

--- app/src/main/java/com/example/assignment2/main/AddActivityActivity.java
+++ app/src/main/java/com/example/assignment2/main/AddActivityActivity.java
@@ -3,14 +3,42 @@ package com.example.assignment2.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.assignment2.R;
 
-public class AddActivityActivity extends AppCompatActivity {
+public class AddActivityActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner bikeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_activity);
+
+        bikeSpinner = findViewById(R.id.selectBikeSpn);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.listOfBikes, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bikeSpinner.setAdapter(adapter);
+
+        bikeSpinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
